879d5881e7b04bf0d5a9e57c428090000c957ab7
Olga Ovchinina <chess.rs56@gmail.com>
2024-04-09 21:06:51 +0300

    level loading mechanics
    when loading a scene, information about objects is added to the CO, in another scene you can recreate all objects

---
+++ Echo-10/Assets/_App/Scripts/Scenes/SceneLevels/Config/ObjectData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets._App.Scripts.Scenes.SceneLevels.Config
+{
+    [Serializable]
+    public class ObjectData
+    {
+        public GameObject objectReference;
+        public string objectName;
+        public Vector3 position;
+        public Vector3 scale;
+    }
+}
---
+++ Echo-10/Assets/_App/Scripts/Scenes/SceneLevels/SceneCreator.cs
@@ -0,0 +1,36 @@
+﻿using Assets._App.Scripts.Scenes.SceneLevels.Config;
+using UnityEngine;
+
+namespace Assets._App.Scripts.Scenes.SceneLevels
+{
+    public class SceneCreator : MonoBehaviour
+    {
+        public ConfigPrefabs objectListSO;
+
+        private void Start()
+        {
+            RecreateSceneFromScriptableObject();
+        }
+
+        public void RecreateSceneFromScriptableObject()
+        {
+            if (objectListSO == null)
+            {
+                Debug.LogError("ObjectListSO is not assigned!");
+                return;
+            }
+
+            foreach (ObjectData data in objectListSO.objects)
+            {
+                if (data.objectReference == null)
+                {
+                    Debug.LogWarning("GameObject is null in ObjectData!");
+                    continue;
+                }
+
+                GameObject newObject = Instantiate(data.objectReference, data.position, Quaternion.identity);
+                newObject.transform.localScale = data.scale;
+            }
+        }
+    }
+}
