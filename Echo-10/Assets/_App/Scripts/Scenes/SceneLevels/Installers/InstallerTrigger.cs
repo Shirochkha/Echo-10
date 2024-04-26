f3b6907dccc1f3f017c351e487dfae1151af9fe5
Olga Ovchinina <chess.rs56@gmail.com>
2024-04-26 19:33:20 +0300

    Added change of alpha channel when colliding with a trigger

--- Echo-10/Assets/_App/Scripts/Scenes/SceneLevels/Installers/InstallerTrigger.cs
+++ Echo-10/Assets/_App/Scripts/Scenes/SceneLevels/Installers/InstallerTrigger.cs
@@ -1,5 +1,6 @@
 ﻿using _App.Scripts.Libs.Installer;
 using _App.Scripts.Libs.ServiceLocator;
+using Assets._App.Scripts.Infrastructure.SceneManagement.Config;
 using Assets._App.Scripts.Scenes.SceneLevels.Features;
 using Assets._App.Scripts.Scenes.SceneLevels.Systems;
 using UnityEngine;
@@ -15,6 +16,10 @@ namespace Assets._App.Scripts.Scenes.SceneLevels.Installers
         [SerializeField] private SphereCollider _colliderComponent;
         [SerializeField] private Text _clickCountText;
 
+        [SerializeField] private float _minAlpha = 0.03f;
+        [SerializeField] private float _maxAlpha = 1f;
+        [SerializeField] private ConfigObjects _configObjects;
+
         public override void InstallBindings(ServiceContainer container)
         {
             var colliderRadiusChange = new SystemColliderRadiusChange(_maxRadius, _duration, _clickCount, 
@@ -23,6 +28,10 @@ namespace Assets._App.Scripts.Scenes.SceneLevels.Installers
 
             var clickCountUI = new ClickCountUI(_clickCountText, colliderRadiusChange);
             container.SetService<IUpdatable, ClickCountUI>(clickCountUI);
+
+            var spriteAlphaChange = new SystemSpriteAlphaChange(_colliderComponent, _minAlpha, _maxAlpha, _duration,
+                colliderRadiusChange, _configObjects);
+            container.SetService<IUpdatable, SystemSpriteAlphaChange>(spriteAlphaChange);
         }
     }
 }
---
+++ Echo-10/Assets/_App/Scripts/Scenes/SceneLevels/Systems/SystemSpriteAlphaChange.cs
@@ -0,0 +1,81 @@
+﻿using _App.Scripts.Libs.Installer;
+using Assets._App.Scripts.Infrastructure.SceneManagement.Config;
+using UnityEngine;
+
+namespace Assets._App.Scripts.Scenes.SceneLevels.Systems
+{
+    public class SystemSpriteAlphaChange : IUpdatable
+    {
+        private SphereCollider _sphereCollider;
+        private float _minAlpha;
+        private float _maxAlpha;
+        private float _maxAlphaDuration;
+        private SystemColliderRadiusChange _colliderRadiusChange;
+        private ConfigObjects _configObjects;
+
+        private bool _isMaxAlpha;
+
+        public SystemSpriteAlphaChange(SphereCollider sphereCollider, float minAlpha, float maxAlpha, 
+            float maxAlphaDuration, SystemColliderRadiusChange colliderRadiusChange, ConfigObjects configObjects)
+        {
+            _sphereCollider = sphereCollider;
+            _minAlpha = minAlpha;
+            _maxAlpha = maxAlpha;
+            _maxAlphaDuration = maxAlphaDuration;
+            _colliderRadiusChange = colliderRadiusChange;
+            _configObjects = configObjects;
+        }
+
+        public void Update()
+        {
+            HandleMaxAlpha();
+            HandleObjectVisibility();
+        }
+
+        private void HandleMaxAlpha()
+        {
+            if (_isMaxAlpha)
+            {
+                _maxAlphaDuration -= Time.deltaTime;
+                if (_maxAlphaDuration <= 0f)
+                {
+                    _isMaxAlpha = false;
+                }
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (_colliderRadiusChange.ClickCount < 10)
+                {
+                    _isMaxAlpha = true;
+                    _maxAlphaDuration = 2f;
+                }
+            }
+        }
+
+        private void HandleObjectVisibility()
+        {
+            foreach (var otherObjectData in _configObjects.objects)
+            {
+                var objRef = otherObjectData.objectReference;
+                if (objRef == null) continue;
+
+                var renderer = objRef.GetComponent<Renderer>();
+                if (renderer == null) continue;
+
+                var shaderName = renderer.material.shader.name;
+                if (shaderName == "Unlit/Texture") continue;
+
+                var isInsideSphere = _sphereCollider.bounds.Contains(renderer.transform.position);
+                var targetAlpha = _isMaxAlpha ? _maxAlpha : (isInsideSphere ? 1f : _minAlpha);
+
+                var currentColor = renderer.material.color;
+                if (Mathf.Approximately(currentColor.a, targetAlpha)) continue;
+
+                currentColor.a = targetAlpha;
+                renderer.material.color = currentColor;
+            }
+        }
+
+    }
+}
